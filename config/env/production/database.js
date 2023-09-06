d689ace2c9a633c9393d72196011ab574d6b0752
souravchandra1910 <souravc399@gmail.com>
2023-09-06 10:05:21 +0530

    added server and database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,20 @@
-module.exports = ({ env }) => ({
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-        },
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
       },
       debug: false,
     },
-  });
+  }
+};
